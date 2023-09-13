908d164308edb238a7dcaa932aadc0999098637b
Kalvin Daniel <kalvin.daniel56300@gmail.com>
2023-09-13 16:24:38 +0200

    init

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine(args[0]);
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Aucun argument n'a été passé.");
+        }else
+        {
+            Console.WriteLine("argument passé.");
+        }
+        var articlesLequipe = await FonctionsScrap.ScrapLequipe();
+        var articlesOneFootball = await FonctionsScrap.ScrapOneFootball();
+        foreach (var article in articlesLequipe)
+        {
+            Console.WriteLine(article.url);
+            Console.WriteLine(article.text);
+        }
+        foreach (var article in articlesOneFootball)
+        {
+            Console.WriteLine(article.url);
+            Console.WriteLine(article.text);
+        }
+    }
+
+}
+
